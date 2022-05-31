adef70f7efd653e61db948872675c8305f4b2f45
yichien <kevinyichienlee@gmail.com>
2022-05-31 23:44:12 +0800

    added basic routers

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import { CssBaseline } from '@mui/material'
-//import { Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 
 const App = () => {
   return (
     <div>
-    <CssBaseline />
-    <h1>Hello, world App</h1>
+      <CssBaseline />
+      <main>
+        <Switch>
+          <Route exact path="/movies/:id">
+            <h1>Movie Information</h1>
+          </Route>
+          <Route exact path="/actors/:id">
+            <h1>Actors</h1>
+          </Route>
+          <Route exact path="/">
+            <h1>Movies</h1>
+          </Route>
+          <Route exact path="/profile/:id">
+            <h1>Profile</h1>
+          </Route>
+        </Switch>
+      </main>
     </div>
   )
 }
