de6f400853b54d32904fafb69a17cd8713776b4b
yichien <kevinyichienlee@gmail.com>
2022-06-04 21:20:21 +0800

    added mediaqury for Menu icon and set isMobile

--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -1,10 +1,22 @@
 import { makeStyles } from "@mui/styles";
 
-export default makeStyles(() => ({
+export default makeStyles((theme) => ({
   toolbar: {
     height: '80px',
     display:'flex',
     justifyContent:'space-between',
     marginLeft:'240px',
+    [theme.breakpoints.down('sm')]: {
+      marginLeft: '0',
+      flexWrap: 'wrap'
   }
+  },
+  // hide icon when it's not mobile
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+        display: 'none',
+    }
+  }
+
 }));
