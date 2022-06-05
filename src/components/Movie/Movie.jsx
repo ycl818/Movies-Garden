5ccf74d6c09be47dfaadb50081e63afc3361225a
yichien <kevinyichienlee@gmail.com>
2022-06-05 15:39:36 +0800

    rendering movie image

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -11,8 +11,14 @@ const Movie = ({movie, i}) => {
   return (
     <Grid item xs={12} sm={6} md={4} lg={3} xl={2} className={classes.movie}>
       <Grow in key={i} timeout={(i + 1) * 500}>
-        <Typography className={classes.title} variant="h5">{movie.title}</Typography>
+        <Link className={classes.links} to={`/movie/${movie.id}`}>
+          {movie.poster_path
+          ? <img alt={movie.title} className={classes.image} src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          : <img alt={movie.title} className={classes.image} src="https://www.fillmurray.com/200/300" />
+          }
+        </Link> 
       </Grow>
+      <Typography className={classes.title} variant="h5">{movie.title}</Typography>
     </Grid>
   );
 }
