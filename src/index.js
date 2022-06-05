ad1445a06f71a1920580b2abddbb9616e7393ef8
yichien <kevinyichienlee@gmail.com>
2022-06-05 11:06:45 +0800

    import store and prepare reducer

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Provider } from "react-redux";
 
 import App from './components/App';
+import store from './app/store';
 
 const theme = createTheme({});
 
