f6c78eb22874c168b06c22005c5d4b8c56668343
yichien <kevinyichienlee@gmail.com>
2022-06-05 11:53:10 +0800

    create MovieList component

---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const MovieList = () => {
+  console.log('movie list'); 
+  return (
+    <div>MovieList</div>
+  )
+}
+
+export default MovieList
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -3,6 +3,7 @@ import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material'
 import { useSelector } from 'react-redux';
 
 import { useGetMoviesQuery } from '../../services/TMDB';
+import MovieList from '..';
 
 const Movies = () => {
   const { data } = useGetMoviesQuery();
@@ -10,7 +11,7 @@ const Movies = () => {
   console.log(data);
 
   return (
-    <div>Movies</div>
+    <MovieList />
   )
 }
 
