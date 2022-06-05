50fb58457fd67c4c3e2128b5d50c3f1e6ccded5e
yichien <kevinyichienlee@gmail.com>
2022-06-05 15:44:10 +0800

    remove redundant code

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -12,10 +12,13 @@ const Movie = ({movie, i}) => {
     <Grid item xs={12} sm={6} md={4} lg={3} xl={2} className={classes.movie}>
       <Grow in key={i} timeout={(i + 1) * 500}>
         <Link className={classes.links} to={`/movie/${movie.id}`}>
-          {movie.poster_path
-          ? <img alt={movie.title} className={classes.image} src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-          : <img alt={movie.title} className={classes.image} src="https://www.fillmurray.com/200/300" />
-          }
+          <img 
+            alt={movie.title} 
+            className={classes.image} 
+            src={movie.poster_path 
+            ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` 
+            : "https://www.fillmurray.com/200/300"} 
+          />
         </Link> 
       </Grow>
       <Typography className={classes.title} variant="h5">{movie.title}</Typography>
