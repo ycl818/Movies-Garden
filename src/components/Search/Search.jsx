8ed8d9c4b4c4903666db5109eb1c0dd71ef1c4a8
yichien <kevinyichienlee@gmail.com>
2022-06-08 20:38:16 +0800

    search funtionality

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -4,14 +4,20 @@ import { Search as SearchIcon } from '@mui/icons-material';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 
+import { searchMovie } from '../../features/currentGenreOrCategory';
 import useStyle from './styles'
 
 const Search = () => {
   console.log("search");
   const classes = useStyle();
   const [query, setQuery] = useState('');
-  const handleKeyPress = () => {
+  const dispatch = useDispatch();
 
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      console.log("here", query);
+      dispatch(searchMovie(query));
+    }
   }
   return (
     <div className={classes.searchContainer}>
