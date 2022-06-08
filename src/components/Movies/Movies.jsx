0606b9383930e16bdca33f93af866358b064e478
yichien <kevinyichienlee@gmail.com>
2022-06-08 19:16:48 +0800

    useSelector and changed a parameter

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
 
+import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
+
 import { useGetMoviesQuery } from '../../services/TMDB';
 import { MovieList } from '..';
 
 const Movies = () => {
-  const { data, error, isFetching } = useGetMoviesQuery();
+  const { genreIdOrCategoryName } = useSelector((state) => state.currentGenreOrCategory);
+  const { data, error, isFetching } = useGetMoviesQuery({ genreIdOrCategoryName });
 
   console.log(data);
   if (isFetching) {
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -20,11 +20,14 @@ const redLogo = 'https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2f
 const blueLogo = 'https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png';
 
 const Sidebar = () => {
+  const { genreIdOrCategoryName } = useSelector((state) => state.currentGenreOrCategory);
   const theme = useTheme();
   const classes = useStyles();
 
   const { data, isFetching } = useGetGenresQuery();
   const dispatch = useDispatch(); // to send or transfer
+
+ 
   
   return (
     <>
