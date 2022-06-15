ce50bf0273a01437ac8e520c2e772206d5d8c9b7
yichien <kevinyichienlee@gmail.com>
2022-06-15 18:45:12 +0800

    render username to profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import {  userSelector } from '../../features/auth'
+// Get access ro profile name or id from redux slice
+// display in the profile component
 
 const Profile = () => {
-
+  const { user } = useSelector(userSelector)
   console.log('Profile');
   return (
-    <div>Profile</div>
+    <div>Profile -{user.username}</div>
   )
 }
 
