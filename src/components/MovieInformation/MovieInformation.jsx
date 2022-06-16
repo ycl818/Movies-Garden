fc875daab028631a262ff8da7fec0864cee92309
yichien <kevinyichienlee@gmail.com>
2022-06-16 16:12:51 +0800

    clear no images actors and show he/she played

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -82,10 +82,14 @@ const MovieInformation = () => {
         </Typography>
         <Grid item container spacing={2}>
             {data && data.credits?.cast?.map((character, i) => (
-              <Grid key={i} item xs={4} md={2} component={Link} to={`/actors/${character.id}}`} style={{textDecoration: 'none'}}>
+              character.profile_path && <Grid key={i} item xs={4} md={2} component={Link} to={`/actors/${character.id}}`} style={{textDecoration: 'none'}}>
                 <img className={classes.castImage} src={`https://image.tmdb.org/t/p/w500/${character.profile_path}`} alt={character.name} />
+                <Typography color="textPrimary">{character?.name}</Typography>
+                <Typography color={"textSecondary"}>
+                  {character.character.split('/')[0]}
+                </Typography>
               </Grid>
-            ))}
+            )).slice(0,6)}
         </Grid>
       </Grid>
     </Grid>
