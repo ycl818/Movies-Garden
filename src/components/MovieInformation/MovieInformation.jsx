b055ce8b2d0ff794387465b34ccd1c46f92f910d
yichien <kevinyichienlee@gmail.com>
2022-06-16 23:03:40 +0800

    small fixed in Language dispaly

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -75,7 +75,7 @@ const MovieInformation = () => {
             {data.vote_average} / 10
           </Box>
           <Typography variant='h6' align='center' gutterBottom>
-            {data?.runtime}min {data?.spoken_languages.length > 0 ? `/ ${data?.spoken_languages[0].name}`:""}
+            {data?.runtime}min | Language:{data?.spoken_languages[0].name}
           </Typography>
         </Grid>
         {/* genres */}
