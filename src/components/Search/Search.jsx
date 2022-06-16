e648251c6030e0b558ba0af90caa769110f77578
yichien <kevinyichienlee@gmail.com>
2022-06-16 23:09:47 +0800

    remove search bar when not in root, and fixed side

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -12,6 +12,7 @@ const Search = () => {
   const classes = useStyle();
   const [query, setQuery] = useState('');
   const dispatch = useDispatch();
+  const location = useLocation();
 
   const handleKeyPress = (event) => {
     if (event.key === 'Enter') {
@@ -19,6 +20,9 @@ const Search = () => {
       dispatch(searchMovie(query));
     }
   }
+
+  if (location.pathname !== '/') return null;
+
   return (
     <div className={classes.searchContainer}>
       <TextField 
