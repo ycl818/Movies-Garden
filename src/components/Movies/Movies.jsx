7fd3097c0a8e4a2e640c1e238d6f3ce004b7b00e
yichien <kevinyichienlee@gmail.com>
2022-06-17 08:30:03 +0800

    removed the first movie repeated

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,8 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
-
-import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
 
 import { useGetMoviesQuery } from '../../services/TMDB';
 import { MovieList, Pagination, FeaturedMovie } from '..';
@@ -14,7 +12,7 @@ const Movies = () => {
 
   const lg = useMediaQuery((theme) => theme.breakpoints.only('lg'));
 
-  const numberOfMovies = lg ? 16 : 18;
+  const numberOfMovies = lg ? 17 : 19;
 
   console.log(data);
   if (isFetching) {
@@ -43,7 +41,7 @@ const Movies = () => {
   return (
     <div>
       <FeaturedMovie movie={data.results[0]}/>
-      <MovieList movies={data} numberOfMovies={numberOfMovies} />
+      <MovieList movies={data} numberOfMovies={numberOfMovies} excludeFirst />
       <Pagination currentPage={page} setPage={setPage} totalPages={data.total_pages}  />
     </div>
   )
