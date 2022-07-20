240bfb1f1fac1ceb309c1b45cb26387169392ef1
yichien <kevinyichienlee@gmail.com>
2022-07-20 21:03:46 +0800

    implement dark / light alan

--- src/components/Alan.jsx
+++ src/components/Alan.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import alanBtn from '@alan-ai/alan-sdk-web';
 
-const Alan = () => {
+
+import { ColorModeContext } from '../utils/ToggleColorMode';
+
+const useAlan = () => {
+
+  const { setMode } =  useContext(ColorModeContext);
+
   useEffect(() => {
     alanBtn({
         key: '57d6f25349ca8deaf47a4e37bfc596c22e956eca572e1d8b807a3e2338fdd0dc/stage',
-        onCommand: (commandData) => {
-          if (commandData.command === 'go:back') {
-            // Call the client code that will react to the received command
+        onCommand: ({command, mode}) => {
+          if (command === 'changeMode') {
+            if (mode ==='light') {
+              setMode('light');
+            } else {
+              setMode('dark');
+            }
           }
         }
     });
   }, []);
-  
-  return (
-    <div>Alan</div>
-  )
+
 }
 
-export default Alan
+export default useAlan;
