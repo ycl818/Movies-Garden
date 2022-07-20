8d2e3e41d7b04e685d0e45e4fb74269d57965440
yichien <kevinyichienlee@gmail.com>
2022-07-20 22:06:57 +0800

    implement search

--- src/components/Alan.jsx
+++ src/components/Alan.jsx
@@ -1,18 +1,36 @@
 import React, { useContext, useEffect } from 'react'
 import alanBtn from '@alan-ai/alan-sdk-web';
 
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+import { selectGenreOrCategory, searchMovie } from '../features/currentGenreOrCategory';
 import { fetchToken} from '../utils';
 import { ColorModeContext } from '../utils/ToggleColorMode';
 
 const useAlan = () => {
 
   const { setMode } =  useContext(ColorModeContext);
+  const dispatch = useDispatch();
+  const history = useHistory();
 
   useEffect(() => {
     alanBtn({
         key: '57d6f25349ca8deaf47a4e37bfc596c22e956eca572e1d8b807a3e2338fdd0dc/stage',
-        onCommand: ({command, mode}) => {
-          if (command === 'changeMode') {
+        onCommand: ({command, mode, genres, genreOrCategory, query}) => {
+          if (command === 'chooseGenre') {
+            const foundGenre = genres.find((g) => g.name.toLowerCase() === genreOrCategory.toLowerCase());
+
+            if (foundGenre) {
+              history.push('/');
+              dispatch(selectGenreOrCategory(foundGenre.id));
+            } else {
+              // top rated upcoming popular
+              const category = genreOrCategory.startsWith('top') ? 'top_rated' : genreOrCategory;
+              history.push('/');
+              dispatch(selectGenreOrCategory(category));
+            }
+          } else if (command === 'changeMode') {
             if (mode ==='light') {
               setMode('light');
             } else {
@@ -23,6 +41,8 @@ const useAlan = () => {
           } else if (command === 'logout') {
             localStorage.clear();
             window.location.href = '/';
+          } else if (command === 'search') {
+            dispatch(searchMovie(query))
           }
 
 
